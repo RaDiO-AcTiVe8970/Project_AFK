3811054fe835fc22b180178fd9bb2ffce33acf54
RaDiO-AcTiVe8970 <mohit.fahid@gmail.com>
2025-07-03 21:32:52 +0600

    initBranchTes

--- project_afk/src/app/pages/hero.js
+++ project_afk/src/app/pages/hero.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 function HeroPage() {
   return (
     <div className="hero flex flex-col items-center justify-center min-h-screen">
-      <Image
-              src="/afk-white.png"
-              alt="Hero Image"
-              width={500}
-              height={300}
-              className="rounded-lg shadow-lg mb-6 z-10"
-            />
+    <Image
+        src="/afk-white.png"
+        alt="Hero Image"
+        width={500}
+        height={300}
+        className="rounded-lg shadow-lg mb-6 z-10"
+    />
     </div>
   );
 }
