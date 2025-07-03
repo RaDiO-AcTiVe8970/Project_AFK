b1556bffbd84f54c7825c1ec5398c7b69ae0f712
RaDiO-AcTiVe8970 <mohit.fahid@gmail.com>
2025-07-03 20:06:36 +0600

    heroInitPush

---
+++ project_afk/src/app/pages/hero.js
@@ -0,0 +1,9 @@
+function HeroPage() {
+  return (
+    <div className="hero">
+      <h1>Welcome to the Hero Page</h1>
+      <p>This is a simple hero section.</p>
+    </div>
+  );
+}
+export default HeroPage;
